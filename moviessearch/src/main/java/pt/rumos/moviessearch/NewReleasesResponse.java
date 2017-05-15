52d865f4c3ae758ad57390aace61607c582470d3
Utilizador <Utilizador@10.48.190.43>
2017-05-15 21:17:10 +0100

    Sample api call with json deserialization

---
+++ moviessearch/src/main/java/pt/rumos/moviessearch/NewReleasesResponse.java
@@ -0,0 +1,29 @@
+/*
+ * To change this license header, choose License Headers in Project Properties.
+ * To change this template file, choose Tools | Templates
+ * and open the template in the editor.
+ */
+package pt.rumos.moviessearch;
+
+import com.fasterxml.jackson.annotation.JsonIgnoreProperties;
+import com.fasterxml.jackson.annotation.JsonProperty;
+import java.util.List;
+
+/**
+ *
+ * @author Utilizador
+ */
+@JsonIgnoreProperties(ignoreUnknown = true)
+public class NewReleasesResponse {
+    
+    @JsonProperty(value = "results")
+    private List<Film> films;
+
+    public List<Film> getFilms() {
+        return films;
+    }
+
+    public void setFilms(List<Film> films) {
+        this.films = films;
+    }
+}
